301ff300069a31df230a1476ad8ecf85776674b4
rivki770 <rivkistudies@gmail.com>
2022-04-10 10:30:24 +0300

    add files to week5part3

---
+++ perfect_dish.py
@@ -0,0 +1,25 @@
+def numbers_perfect_dish() -> int:
+    '''
+    The function move number number and check if sum of all his divisions equal himself.
+    For example:
+    6 is perfect dish: 1 + 2 + 3 = 6.
+    8 is not perfect dish: 1 + 2 + 4 != 8.
+
+    :return: The number that sum of all his divisions equal himself.
+    '''
+    number_to_check = 1
+    while True:
+        sum_list_of_divide = sum([i for i in range(1, number_to_check) if number_to_check % i == 0])
+        if sum_list_of_divide == number_to_check:
+            yield sum_list_of_divide
+        number_to_check = number_to_check + 1
+
+
+def main():
+    perfect_dish_numbers = numbers_perfect_dish()
+    while True:
+        print(next(perfect_dish_numbers))
+
+
+if __name__ == '__main__':
+    main()
