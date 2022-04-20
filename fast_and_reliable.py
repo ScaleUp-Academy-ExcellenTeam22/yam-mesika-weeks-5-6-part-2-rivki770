a566d8023b9767c080cd4614ba6b0b42e35e14c7
rivki770 <rivkistudies@gmail.com>
2022-04-20 14:00:03 +0300

    Fix comments

--- fast_and_reliable.py
+++ fast_and_reliable.py
@@ -2,24 +2,23 @@ import math
 import time
 
 
-def average_runtime(words):
+def average_runtime(list_or_set_of_words):
     """
     A function that receives a list of words and returns how long it takes on average to search for a specific word.
-    :param words: or set of words or list of words
+    :param list_or_set_of_words: A set or a list of words.
     :return: the average-time to find "zwitterion" in a set or a list
     """
-    time_first = time.time()
+    time_start = time.time()
     for i in range(0, 1000):
-        if "zwitterion" in words:
+        if "zwitterion" in list_or_set_of_words:
             continue
-    time_second = time.time()
-    return (time_second - time_first) / 1000
+    return (time.time() - time_start) / 1000
 
 
 def open_file(path_to_file: str):
     """
-    A function that gets a path to a file, and copy the line from a file to list and set,
-    and she call to inner function.
+    A function that gets a path to a file, and copies the words / text from the file to a list and a set,
+    and it call to inner function that looking for a specific word in the list or set a function created.
     :param path_to_file: path to file.
     :return: nothing.
     """
@@ -28,13 +27,11 @@ def open_file(path_to_file: str):
 
     try:
         with open(path_to_file, "r") as file:
-            word = file.readline(1)
-            while word:
+            for word in file:
                 word_list.append(word)
                 word_set.add(word)
-                word = file.readline(1)
     except IOError:
-        print('Error While Opening the file!')
+        print('Error while opening the file!')
 
     print(f"The time for find zwitterion in list: {average_runtime(word_list)}")
     print(f"The time for find zwitterion in set: {average_runtime(word_set)}")
--- stuhlandia.py
+++ stuhlandia.py
@@ -1,7 +1,7 @@
 def state_in_line_on_keyboard(path_to_file_of_country: str):
     """
     A function that gets a path to a file with countries,
-    looks for a country that can type in one line on the keyboard.
+    and searches for a country that can be written using letters from a single line of the keyboard.
     :param path_of_file_of_country: path to file of states.
     :return: nothing.
     """
@@ -10,22 +10,20 @@ def state_in_line_on_keyboard(path_to_file_of_country: str):
     third_keyboard_row = set('zxcvbnm\n')
 
     try:
-        with open(path_to_file_of_country, "r") as file_country:
-            state = file_country.readline()
-            while state:
-                start_set = set(state)
-                if len(set(start_set.difference(first_keyboard_row))) == 0:
+        with open(path_to_file_of_country, "r") as file_states:
+            for state in file_states:
+                state_set = set(state)
+                if len(set(state_set.difference(first_keyboard_row))) == 0:
                     print(f'In row 1: {state}')
                     return
-                elif len(set(start_set.difference(second_keyboard_row))) == 0:
+                elif len(set(state_set.difference(second_keyboard_row))) == 0:
                     print(f'In row 2: {state}')
                     return
-                elif len(set(start_set.difference(third_keyboard_row))) == 0:
+                elif len(set(state_set.difference(third_keyboard_row))) == 0:
                     print(f'In row 3: {state}')
                     return
-                state = file_country.readline()
     except IOError:
-        print('Error While Opening the file!')
+        print('Error while opening the file!')
 
 
 def main():
