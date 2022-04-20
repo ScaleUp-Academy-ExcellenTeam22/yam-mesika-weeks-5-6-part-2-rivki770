b37f73a5e5b7c89eb534c8ac95ffcf021c932efd
rivki770 <rivkistudies@gmail.com>
2022-04-20 14:15:38 +0300

    Fix comments

--- tool.py
+++ tool.py
@@ -1,15 +1,19 @@
-def interleave(*iterable):
+def interleave(*collection_of_iterables):
     """
-     A function that receives a list of lists and returns a merge between the lists
-    :param iterable: Unlimited list of objects.
+     A function that receives a list of lists and returns a merge between the lists.
+     (merge: the first object of all, the second object of all...)
+     For example:
+        input: 'abc', [1, 2, 3], ('!', '@', '#').
+        output: 'a', 1, '!', 'b', 2, '@', 'c', 3, '#'
+
+    :param collection_of_iterables: Unlimited list of objects, any object is type of iterable.
     :return: In each reading the object in the next position
     """
-    size_min = min(len(i) for i in iterable)
-
-    current_number = 0
-    while current_number < size_min:
-        for i in iterable:              # move on all a list
-            yield i[current_number]
+    minimum_collection_size = min(len(iterable) for iterable in collection_of_iterables)
+    current_index = 0
+    while current_index < minimum_collection_size:
+        for iterable in collection_of_iterables:              # move on all a list
+            yield iterable[current_index]
         current_number += 1
 
 
